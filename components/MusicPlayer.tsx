7c80891033b9ed9ca0047baeaa7ac0bb4bce9005
UnicornDad8 <cecibenitezca@gmail.com>
2025-10-29 14:59:06 -0300

    Add togglePlayButton functionality

--- components/MusicPlayer.tsx
+++ components/MusicPlayer.tsx
@@ -1,11 +1,33 @@
+"use client"
+
+import { useRef, useState } from "react"
 import Image from "next/image"
-import { IoMdPlay, IoMdSkipBackward, IoMdSkipForward, IoMdVolumeHigh } from "react-icons/io"
+import { IoMdPause, IoMdPlay, IoMdSkipBackward, IoMdSkipForward, IoMdVolumeHigh } from "react-icons/io"
 import { LuRepeat1 } from "react-icons/lu"
 import { MdOutlineQueueMusic } from "react-icons/md"
 
 const MusicPlayer = () => {
+  const audioRef = useRef<HTMLAudioElement | null>(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [volume, setVolume] = useState(50);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [duration, setDuration] = useState(0);
+
+  const togglePlayButton = () => {
+    if(!audioRef.current) return;
+
+    if(isPlaying) {
+      audioRef.current.pause(); 
+    } else {
+      audioRef.current.play();
+    }
+
+    setIsPlaying(!isPlaying);
+  }
+
   return (
     <div className="fixed bottom-0 left-0 w-full bg-black text-white px-4 py-3 shadow-md z-50">
+      <audio src="/audio/bailando-bachata.mp3" controls ref={audioRef}></audio>
       <div className="max-w-8xl w-[95%] mx-auto flex items-center justify-between">
         <div className="flex gap-4 items-center">
           <Image src="/images/cover-2.jpeg" alt="cover 2" width={300} height={300} className="w-13 h-13 object-cover rounded-md" />
@@ -19,8 +41,11 @@ const MusicPlayer = () => {
             <button className="text-xl text-secondary-text">
               <IoMdSkipBackward />
             </button>
-            <button className="bg-white text-xl text-black w-10 h-10 rounded-full grid place-items-center">
-              <IoMdPlay />
+            <button 
+              className="bg-white text-xl text-black w-10 h-10 rounded-full grid place-items-center"
+              onClick={togglePlayButton}
+            >
+              {isPlaying ? <IoMdPause /> : <IoMdPlay />}
             </button>
             <button className="text-xl text-secondary-text">
               <IoMdSkipForward />
