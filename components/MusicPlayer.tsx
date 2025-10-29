55c04ef21939d50eaeb3813aab402eaee271b027
UnicornDad8 <cecibenitezca@gmail.com>
2025-10-29 15:51:21 -0300

    Add set volume and mute functionality

--- components/MusicPlayer.tsx
+++ components/MusicPlayer.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useRef, useState, ChangeEvent } from "react"
 import Image from "next/image"
-import { IoMdPause, IoMdPlay, IoMdSkipBackward, IoMdSkipForward, IoMdVolumeHigh } from "react-icons/io"
+import { IoMdPause, IoMdPlay, IoMdSkipBackward, IoMdSkipForward, IoMdVolumeHigh, IoMdVolumeOff } from "react-icons/io"
 import { LuRepeat1 } from "react-icons/lu"
 import { MdOutlineQueueMusic } from "react-icons/md"
 
@@ -12,6 +12,7 @@ const MusicPlayer = () => {
   const [volume, setVolume] = useState(50);
   const [currentTime, setCurrentTime] = useState(0);
   const [duration, setDuration] = useState(0);
+  const [previousVolume, setPreviousVolume] = useState(0);
 
   const togglePlayButton = () => {
     if(!audioRef.current) return;
@@ -54,6 +55,38 @@ const MusicPlayer = () => {
       setCurrentTime(newTime);
     }
   }
+
+  const handleVolumeChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const vol = parseInt(e.target.value);
+    setVolume(vol);
+
+    if(audioRef.current) {
+      audioRef.current.volume = vol / 100;
+    }
+  }
+
+  const toggleMute = () => {
+    if(volume === 0) {
+      setVolume(previousVolume);
+
+      if(audioRef.current) {
+        audioRef.current.volume = previousVolume / 100;
+      }
+    } else {
+      setPreviousVolume(volume);
+      setVolume(0);
+
+      if(audioRef.current) {
+        audioRef.current.volume = 0;
+      }
+    }
+  }
+
+  useEffect(() => {
+    if(audioRef.current) {
+      audioRef.current.volume = volume / 100;
+    }
+  }, [volume]);
 
   return (
     <div className="fixed bottom-0 left-0 w-full bg-black text-white px-4 py-3 shadow-md z-50">
@@ -103,10 +136,22 @@ const MusicPlayer = () => {
             <button className="text-secondary-text text-xl cursor-pointer">
               <MdOutlineQueueMusic />
             </button>
-            <button className="text-secondary-text text-xl cursor-pointer">
-              <IoMdVolumeHigh />
+            { volume === 0 ?
+               <button className="text-secondary-text text-xl cursor-pointer">
+               <IoMdVolumeOff onClick={toggleMute} />
+             </button> :
+              <button className="text-secondary-text text-xl cursor-pointer">
+              <IoMdVolumeHigh onClick={toggleMute} />
             </button>
-            <input type="range" min="0" max="100" className="w-[100px] outline-none h-1 bg-zinc-700 accent-white appearance-none" />
+            }
+            <input 
+              onChange={handleVolumeChange}
+              value={volume}
+              type="range" 
+              min="0" 
+              max="100" 
+              className="w-[100px] outline-none h-1 bg-zinc-700 accent-white appearance-none" 
+            />
           </div>
       </div>
     </div>
